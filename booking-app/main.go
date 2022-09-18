fddb4f882a4e90637ea3b3910c4f37da3549aca9
Felipe Maeda <maedaxd@gmail.com>
2022-09-18 05:47:54 -0300

    Update main.go

--- booking-app/main.go
+++ booking-app/main.go
@@ -5,4 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to our conference booking application")
 	fmt.Println("Get your tickets here to attend")
+
+	var conferenceName = "Go Conference"
 }
